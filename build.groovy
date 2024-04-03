09946f323fdf17d43f528d500d16485ba7cf87ec
Bharath <134907687+BharathJayadev@users.noreply.github.com>
2024-04-03 10:50:44 +0530

    Create build.groovy

---
+++ build.groovy
@@ -0,0 +1,12 @@
+def compile(){
+  sh "mvn compile"
+}
+def test(){
+  sh "mvn test"
+}
+def qa(){
+  sh "mvn checkstyle:checkstyle"
+}
+def build(){
+  sh "sudo mv -f /var/lib/jenkins/workspace/project_address_book/target/addressbook.war /home/ubuntu/apache-tomcat-8.5.100/webapps"
+}
